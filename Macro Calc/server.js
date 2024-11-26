2c3ec4b805df5e2bfdc4e8c9a31ebd72e727a0d7
Jthompson713 <thompson.jared96@gmail.com>
2024-11-25 22:41:45 -0800

    Reup

--- Macro Calc/server.js
+++ Macro Calc/server.js
@@ -13,9 +13,7 @@ app.use((req, res, next) => {
 
 // CORS configuration
 app.use(cors({
-    origin: process.env.NODE_ENV === 'production' 
-        ? ['https://jthompson713.github.io/recipe-calc/', 'https://www.jthompson713.github.io/recipe-calc/']
-        : '*',
+    origin: ['https://jthompson713.github.io'],
     methods: ['GET', 'POST'],
     credentials: true
 }));
