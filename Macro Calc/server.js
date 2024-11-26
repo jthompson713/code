e3df2f218f7f0f1eb0ac8e2fcc20c931c4167b6a
Jthompson713 <thompson.jared96@gmail.com>
2024-11-25 23:11:55 -0800

    Added port listening

--- Macro Calc/server.js
+++ Macro Calc/server.js
@@ -91,6 +91,10 @@ app.use((err, req, res, next) => {
     });
 });
 
+app.listen(port, "0.0.0.0", ()=>{
+    console.log(`Server listening to port ${port}`);
+});
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
